f27b96a1c82a04d87041c613d827171c5723b19e
Ennie <califpornia@ya.ru>
2011-07-08 09:52:54 +0600

    Initial commit, based on zork`s rFilter3: * ToC update for 4.2 * Paladin spells for retribution tree

---
+++ config.lua
@@ -0,0 +1,384 @@
+﻿-- // cFilter by Califpornia. Based on zork`s rFilter3
+-- // rFilter3
+-- // zork - 2010
+
+--get the addon namespace
+local addon, ns = ...
+
+--object container
+local cfg = CreateFrame("Frame") 
+
+cfg.rf3_BuffList, cfg.rf3_DebuffList, cfg.rf3_CooldownList = {}, {}, {}
+
+local player_name, _ = UnitName("player")
+_, cfg.player_class = UnitClass("player")
+
+-----------------------------
+-- CONFIG
+-----------------------------
+
+cfg.highlightPlayerSpells = true
+cfg.updatetime			= 0.2 --how fast should the timer update itself
+
+
+-- Califpornia addition
+-- anchor point for all icons
+cfg.readytext = "rdy"
+cfg.itemouttext = "out"
+cfg.apos = {"TOPLEFT", "UIParent", "CENTER", -204, -144}
+cfg.growth_x = "RIGHT"
+cfg.growth_y = "UP"
+cfg.space_x = 2
+cfg.space_y = 2
+cfg.rowicons = 12
+cfg.size = 32
+cfg.desaturate = true
+cfg.alpha = {
+	cooldown = {
+		frame = 1,
+		icon = 0.6,
+	},
+	no_cooldown = {
+		frame = 1,
+		icon = 1,		
+	},
+	found = {
+		frame = 1,
+		icon = 1,
+	},
+	not_found = {
+		frame = 0.4,
+		icon = 0.6,          
+	},
+}
+cfg.track_class = true
+cfg.track_enchants = false
+cfg.track_items = true
+cfg.track_equip_slots = true
+cfg.timefont = {"Interface\\AddOns\\CalifporniaUI\\media\\fonts\\big_noodle_tilting.ttf", 14, "OUTLINE"}
+cfg.IconList = {
+	["ENCHANTS"] = {
+		-- MH
+		{
+			enabled		= true,
+			type			= "enchant",
+			spec			= nil, 
+			spellid		= 51730,
+		        spelllist		= nil,
+			cd_itemid		= nil,
+			inv_slot		= 16,
+		},
+		-- OH
+		{
+			enabled		= true,
+			type			= "enchant",
+			spec			= nil, 
+			spellid		= 51730,
+		        spelllist		= nil,
+			cd_itemid		= nil,
+			inv_slot		= 17,
+		},
+		-- Thrown
+		{
+			enabled		= true,
+			type			= "enchant",
+			spec			= nil, 
+			spellid		= 51730,
+		        spelllist		= nil,
+			cd_itemid		= nil,
+			inv_slot		= 18,
+		},
+	},
+	["ITEMS"] = {
+		-- potions
+	},
+	["PALADIN"] = {
+		-- Common
+		-- Buffs
+		-- GoaK
+		{
+			enabled		= true,
+			type			= "multi",
+			spec			= nil, 
+			spellid		= 86150,
+			unit			= "player",
+		},
+		-- Avenging Wrath
+		{
+			enabled		= true,
+			type			= "multi",
+			spec			= nil, 
+			spellid		= 31884,
+			unit			= "player",
+		},
+		-- Inquisition
+		{
+			enabled		= true,
+			type			= "buff",
+			spec			= 1, 
+			spellid		= 84963,
+			unit			= "player",
+		},
+		-- Zealotry
+		{
+			enabled		= true,
+			type			= "multi",
+			spec			= 1, 
+			spellid		= 85696,
+			unit			= "player",
+		},
+		-- Divine Purpose
+		{
+			enabled		= true,
+			type			= "buff",
+			spec			= 1, 
+			spellid		= 90174,
+			unit			= "player",
+		},
+		-- Art of War
+		{
+			enabled		= true,
+			type			= "buff",
+			spec			= 1, 
+			spellid		= 59578,
+			unit			= "player",
+		},
+		-- Enlightened Judgements
+		{
+			enabled		= true,
+			type			= "buff",
+			spec			= nil, 
+			spellid		= 53657,
+		        spelllist		= {
+				53655,	-- rank 1
+				53656,	-- rank 2
+			},
+			unit			= "player",
+		},
+		-- Censure
+		{
+			enabled		= true,
+			type			= "debuff",
+			spec			= nil, 
+			spellid		= 31803,
+			unit			= "target",
+			validate_unit	= true,
+			ismine		= true,
+		},
+		-- Forbearance
+		{
+			enabled		= true,
+			type			= "debuff",
+			spec			= nil, 
+			spellid		= 25771,
+			unit			= "player",
+			validate_unit	= true,
+		},
+		-- Cata STR pot
+		{
+			enabled		= true,
+			type			= "multi",
+			spec			= nil, 
+			spellid		= 79634,
+		        spelllist		= nil,
+			cd_itemid		= 58146,
+			cd_slotid		= nil,
+			unit			= "player",
+			validate_unit	= false,
+			hide_ooc		= false,
+			ismine		= false,
+		},
+		-- Eng gloves
+		{
+			enabled		= true,
+			type			= "multi",
+			spec			= nil, 
+			spellid		= 96229,
+			cd_itemid		= GetInventoryItemID("player", 10),
+			cd_slotid		= nil,
+			unit			= "player",
+			validate_unit	= false,
+			hide_ooc		= false,
+			ismine		= false,
+		},
+		-- Seals
+		{
+			enabled		= true,
+			type			= "buff",
+			spec			= nil, 
+			spellid		= 31801, -- Seal of Truth
+		        spelllist		= {
+				20154,	-- Seal of Righteousness
+				20164,	-- Seal of Justice
+				20165,	-- Seal of Insight
+				31801,	-- Seal of Truth
+			},
+			unit			= "player",
+		},
+	},
+	["SHAMAN"] = {
+		-- Common
+		-- Shields
+		{
+			enabled		= true,
+			type			= "buff",
+			spec			= nil, 
+			spellid		= 324,	-- lightning shield
+		        spelllist		= {
+				324,
+				52127,	-- water shield
+				974,	-- earth shield on self
+			},
+			unit			= "player",
+		},
+	},
+	["WARRIOR"] = {
+		-- Common
+		-- Sunder Armor
+		{
+			enabled		= true,
+			type			= "debuff",
+			spec			= nil, 
+			spellid		= 58567,
+			unit			= "target",
+			validate_unit	= true,
+		},
+	},
+	["HUNTER"] = {
+		-- Common
+		-- Sunder Armor
+		{
+			enabled		= true,
+			type			= "debuff",
+			spec			= nil, 
+			spellid		= 1978,
+			unit			= "target",
+			validate_unit	= true,
+			ismine		= true,
+		},
+	},
+	["MAGE"] = {
+		-- Blink
+		{
+			enabled		= true,
+			-- multi = cd+duration, debuff = debuff, buff = buff, cd = cooldown :)
+			type			= "multi",
+			spec			= nil, 
+			spellid		= 1953,
+			-- If aura have analogs check them all, because other classes can do the same (c)
+		        spelllist		= nil,
+			-- if you wish track things like pots and/or eng enchants you should put item ID or slot here
+			cd_itemid		= nil,
+			cd_slotid		= nil,
+			unit			= "player",
+			-- only show the icon if unit is found
+			validate_unit	= false,
+			 -- hide icon out of combat
+			hide_ooc		= false,
+			ismine		= false,
+		},
+		-- Mirror Image
+		{
+			enabled		= true,
+			type			= "multi",
+			spec			= nil, 
+			spellid		= 55342,
+		        spelllist		= {70747},	-- 4t10 bonus
+			cd_itemid		= nil,
+			cd_slotid		= nil,
+			unit			= "player",
+			validate_unit	= false,
+			hide_ooc		= false,
+			ismine		= false,
+		},
+		-- Ice Block
+		{
+			enabled		= true,
+			type			= "multi",
+			spec			= nil, 
+			spellid		= 45438,
+		        spelllist		= nil,
+			cd_itemid		= nil,
+			cd_slotid		= nil,
+			unit			= "player",
+			validate_unit	= false,
+			hide_ooc		= false,
+			ismine		= false,
+		},
+		-- Mana Gem
+		{
+			enabled		= true,
+			type			= "multi",
+			spec			= nil, 
+			spellid		= 5405,
+		        spelllist		= {83098},	-- Improved Mana Gem
+			cd_itemid		= 36799,
+			cd_slotid		= nil,
+			unit			= "player",
+			validate_unit	= false,
+			hide_ooc		= false,
+			ismine		= false,
+		},
+		-- DEBUFFS
+		-- slow
+		{
+			enabled		= true,
+			type			= "debuff",
+			spec			= nil, 
+			spellid		= 31589,
+		        spelllist		= nil,
+			cd_itemid		= nil,
+			cd_slotid		= nil,
+			unit			= "target",
+			validate_unit	= true,
+			hide_ooc		= false,
+			ismine		= false,
+		},
+	}
+}
+
+--[[
+Regular inventory items
+0 = ammo 
+1 = head 
+2 = neck 
+3 = shoulder 
+4 = shirt 
+5 = chest 
+6 = belt 
+7 = legs 
+8 = feet 
+9 = wrist 
+10 = gloves 
+11 = finger 1 
+12 = finger 2 
+13 = trinket 1 
+14 = trinket 2 
+15 = back 
+16 = main hand 
+17 = off hand 
+18 = ranged 
+19 = tabard 
+20 = first bag (the rightmost one) 
+21 = second bag 
+22 = third bag 
+23 = fourth bag (the leftmost one) 
+
+When bank frame is open
+40 to 67 = the 28 bank slots 
+68 = first bank bag slot 
+69 = second bank bag slot 
+70 = third bank bag slot 
+71 = fourth bank bag slot 
+72 = fifth bank bag slot 
+73 = sixth bank bag slot 
+74 = seventh bank bag slot
+]]
+
+
+-----------------------------
+-- HANDOVER
+-----------------------------
+
+--object container to addon namespace
+ns.cfg = cfg
---
+++ core.lua
@@ -0,0 +1,409 @@
+﻿
+-- // rFilter3
+-- // zork - 2010
+
+--get the addon namespace
+local addon, ns = ...
+
+--get the config
+local cfg = ns.cfg
+
+
+-- califpornia
+local cFilterIcons = {}
+local cFilterIconRows = {}
+local cFilterRowCount = 1
+local cFilterRowIconCount = 0
+-----------------------------
+-- FUNCTIONS
+-----------------------------
+
+--format time func
+local GetFormattedTime = function(time)
+	local text
+	local d, h, m, s = ChatFrame_TimeBreakDown(time);
+	if( time <= 0 ) then
+		text = ""
+	elseif( time < 3600 and time >= 60) then
+		text = format("%02d:%02d", m, s);
+	elseif( time < 60 ) then
+		text = format("%02ds", s);
+	else
+		text = format("%02d:%02d", h, m);
+	end
+	return text
+end
+
+-- califpornia
+local cSetIconPosition = function(icon)
+	if cFilterRowCount == 1 and cFilterRowIconCount == 0 then
+		icon:SetPoint(unpack(cfg.apos))
+		cFilterIconRows[cFilterRowCount] = {}
+		cFilterRowIconCount = 1
+	elseif cFilterRowIconCount == cfg.rowicons then
+		if cfg.growth_y == "DOWN" then
+			icon:SetPoint("TOP", cFilterIconRows[cFilterRowCount][1], "BOTTOM", 0, -cfg.space_y)
+		else
+			icon:SetPoint("BOTTOM", cFilterIconRows[cFilterRowCount][1], "TOP", 0, cfg.space_y)
+		end
+		cFilterRowCount = cFilterRowCount + 1
+		cFilterRowIconCount = 1
+		cFilterIconRows[cFilterRowCount] = {}
+	else
+		if cfg.growth_x == "RIGHT" then
+			icon:SetPoint("LEFT", cFilterIconRows[cFilterRowCount][cFilterRowIconCount], "RIGHT", cfg.space_x, 0)
+		else
+			icon:SetPoint("RIGHT", cFilterIconRows[cFilterRowCount][cFilterRowIconCount], "LEFT", -cfg.space_x, 0)
+		end
+		cFilterRowIconCount = cFilterRowIconCount + 1
+	end
+	cFilterIconRows[cFilterRowCount][cFilterRowIconCount] = icon
+end
+
+local createIcon = function(f)
+	if not f.enabled then return end
+
+	local w = cfg.size
+
+	local i = CreateFrame("FRAME",nil,UIParent)
+	i:SetSize(w, w)
+	cSetIconPosition(i)
+
+--	if not f.spellid and f.cd_itemid then
+--		local itm_spell = GetItemSpell(f.cd_itemid)
+--		f.spellid = 
+--	end
+	local gsi_name, gsi_rank, gsi_icon, gsi_powerCost, gsi_isFunnel, gsi_powerType, gsi_castingTime, gsi_minRange, gsi_maxRange = GetSpellInfo(f.spellid)
+	
+	
+	local gl = i:CreateTexture(nil, "BACKGROUND",nil,-8)
+	gl:SetPoint("TOPLEFT",i,"TOPLEFT",-w*3.3/32,w*3.3/32)
+	gl:SetPoint("BOTTOMRIGHT",i,"BOTTOMRIGHT",w*3.3/32,-w*3.3/32)
+	gl:SetTexture("Interface\\AddOns\\CalifporniaUI_ClassTimers\\media\\simplesquare_glow")
+	gl:SetVertexColor(0, 0, 0, 1)
+
+	local ba = i:CreateTexture(nil, "BACKGROUND",nil,-7)
+	ba:SetAllPoints(i)
+	ba:SetTexture("Interface\\AddOns\\CalifporniaUI_ClassTimers\\media\\d3portrait_back2")
+	
+	local t = i:CreateTexture(nil,"BACKGROUND",nil,-6)
+	t:SetPoint("TOPLEFT",i,"TOPLEFT",w*3/32,-w*3/32)
+	t:SetPoint("BOTTOMRIGHT",i,"BOTTOMRIGHT",-w*3/32,w*3/32)
+	t:SetTexture(gsi_icon)
+	t:SetTexCoord(0.1,0.9,0.1,0.9)
+	if cfg.desaturate then
+		t:SetDesaturated(1)
+	end
+
+	local bo = i:CreateTexture(nil,"BACKGROUND",nil,-4)
+	bo:SetTexture("Interface\\AddOns\\CalifporniaUI_ClassTimers\\media\\simplesquare_roth")
+	bo:SetVertexColor(0.37,0.3,0.3,1)
+	bo:SetAllPoints(i)
+	
+	local time = i:CreateFontString(nil, "BORDER")
+	time:SetFont(unpack(cfg.timefont))
+	time:SetPoint("BOTTOM", 0, 0)
+	time:SetTextColor(1, 0.8, 0)
+	--time:SetShadowColor(0,0,0,1)
+	--time:SetShadowOffset(w*1/32, -w*1/32)
+	
+	local count = i:CreateFontString(nil, "BORDER")
+	count:SetFont(unpack(cfg.timefont))
+	count:SetPoint("TOPRIGHT", 0,0)
+	--count:SetShadowColor(0,0,0,1)
+	--count:SetShadowOffset(-w*1/32, -w*1/32)
+	count:SetTextColor(1, 1, 1)
+	count:SetJustifyH("RIGHT")
+	
+	i.glow = gl
+	i.border = bo
+	i.back = ba
+	i.time = time
+	i.count = count
+	i.icon = t
+	f.iconframe = i
+	f.buff_active = false		-- only for multi icons
+	f.name = gsi_name
+	f.rank = gsi_rank
+	f.texture = gsi_icon	
+
+end
+
+
+local cUpdateEntry = function(entry)
+	if not entry.enabled then
+		entry.iconframe:SetAlpha(0)
+		return
+	end
+	if entry.spec and entry.spec ~= GetActiveTalentGroup() then
+		entry.iconframe:SetAlpha(0)
+		return
+	end
+	if not UnitExists(entry.unit) and entry.validate_unit then
+		entry.iconframe:SetAlpha(0)
+		return
+	end
+	if not InCombatLockdown() and entry.hide_ooc then
+		entry.iconframe:SetAlpha(0)
+		return
+	end
+
+	-- update buffs and debuffs
+	if entry.type == "multi" or entry.type == "buff" or entry.type == "debuff" then
+		local ua_filter
+		if entry.type == "debuff" then
+			ua_filter = "HARMFUL"
+		else
+			ua_filter = "HELPFUL"
+		end
+		local name, rank, icon, count, dispelType, duration, expires, caster, isStealable, shouldConsolidate, spID
+		local a_found = false
+		if entry.spelllist and entry.spelllist[1] then
+			for i=1, #entry.spelllist, 1 do
+				local gsi_name, gsi_rank, gsi_icon = GetSpellInfo(entry.spelllist[i])
+				if gsi_name then
+					name, rank, icon, count, dispelType, duration, expires, caster, isStealable, shouldConsolidate, spID = UnitAura(entry.unit, gsi_name, gsi_rank, ua_filter)
+					if name and (not entry.ismine or (entry.ismine and caster == "player")) then
+						entry.name = gsi_name
+						entry.rank = gsi_rank
+						entry.texture_list = gsi_icon
+						entry.iconframe.icon:SetTexture(entry.texture_list)
+						a_found = true
+						break
+					end
+				end
+			end
+		end
+		if not a_found then
+			name, rank, icon, count, dispelType, duration, expires, caster, isStealable, shouldConsolidate, spID = UnitAura(entry.unit, entry.name, entry.rank, ua_filter)
+			if name and (not entry.ismine or (entry.ismine and caster == "player")) then
+				a_found = true
+			end
+		end
+		if a_found then
+			if caster == "player" and cfg.highlightPlayerSpells then
+				entry.iconframe.border:SetVertexColor(0.2,0.6,0.8,1)
+			elseif cfg.highlightPlayerSpells then
+				entry.iconframe.border:SetVertexColor(0.37,0.3,0.3,1)
+			end
+			entry.iconframe.icon:SetAlpha(cfg.alpha.found.icon)
+			entry.iconframe:SetAlpha(cfg.alpha.found.frame)
+			if cfg.desaturate then
+				entry.iconframe.icon:SetDesaturated(nil)
+			end
+			if count and count > 1 then
+				entry.iconframe.count:SetText(count)
+			else
+				entry.iconframe.count:SetText("")
+			end
+			local value = expires-GetTime()
+			if value < 10 then
+				entry.iconframe.time:SetTextColor(1, 0.4, 0)
+			else
+				entry.iconframe.time:SetTextColor(1, 0.8, 0)
+			end
+			entry.iconframe.time:SetText(GetFormattedTime(value))
+
+		else
+			entry.iconframe:SetAlpha(cfg.alpha.not_found.frame)
+			entry.iconframe.icon:SetAlpha(cfg.alpha.not_found.icon)
+			if entry.spelllist and entry.spelllist[1] then
+				entry.iconframe.icon:SetTexture(entry.texture)
+			end
+			entry.iconframe.time:SetText("")
+			entry.iconframe.count:SetText("")
+			entry.iconframe.time:SetTextColor(1, 0.8, 0)
+			if cfg.highlightPlayerSpells then
+				entry.iconframe.border:SetVertexColor(0.37,0.3,0.3,1)
+			end 
+			if cfg.desaturate then
+				entry.iconframe.icon:SetDesaturated(1)
+			end
+		end
+		entry.buff_active = a_found
+	end
+
+	-- update cooldown
+	if (entry.type == "multi" and not entry.buff_active) or entry.type == "cd" then
+		local start, duration, enable
+		if entry.cd_itemid then
+			start, duration, enable = GetItemCooldown(entry.cd_itemid)
+		else
+			start, duration, enable = GetSpellCooldown(entry.spellid)
+		end
+		if start and duration then
+			local now = GetTime()		
+			local value = start+duration-now
+			if(value > 0) and duration > 2 then
+				-- item is on cooldown show time
+				entry.iconframe.icon:SetAlpha(cfg.alpha.cooldown.icon)
+				entry.iconframe:SetAlpha(cfg.alpha.cooldown.frame)
+				entry.iconframe.count:SetText("")
+				entry.iconframe.border:SetVertexColor(0.37,0.3,0.3,1)
+				if cfg.desaturate then
+					entry.iconframe.icon:SetDesaturated(1)
+				end
+				if value < 10 then
+					entry.iconframe.time:SetTextColor(1, 0.4, 0)
+				else
+					entry.iconframe.time:SetTextColor(1, 0.8, 0)
+				end
+				entry.iconframe.time:SetText(GetFormattedTime(value))
+			elseif entry.cd_itemid and GetItemCount(entry.cd_itemid, nil, true) == 0 then
+				entry.iconframe.icon:SetAlpha(cfg.alpha.cooldown.icon)
+				entry.iconframe:SetAlpha(cfg.alpha.cooldown.frame)
+				entry.iconframe.count:SetText("")
+				entry.iconframe.border:SetVertexColor(0.37,0.3,0.3,1)
+				if cfg.desaturate then
+					entry.iconframe.icon:SetDesaturated(1)
+				end
+				entry.iconframe.time:SetTextColor(1, 0.4, 0)
+				entry.iconframe.time:SetText(cfg.itemouttext)
+			else
+				entry.iconframe:SetAlpha(cfg.alpha.no_cooldown.frame)
+				entry.iconframe.icon:SetAlpha(cfg.alpha.no_cooldown.icon)
+				entry.iconframe.time:SetText(cfg.readytext)
+				entry.iconframe.count:SetText("")
+				entry.iconframe.time:SetTextColor(0, 0.8, 0)
+				entry.iconframe.border:SetVertexColor(0.4,0.6,0.2,1)
+				if cfg.desaturate then
+					entry.iconframe.icon:SetDesaturated(nil)
+				end
+			end
+		end
+		-- check item in inventory
+	end
+	-- temp weapon enchants like poisons
+	if entry.type == "enchant" then
+		if not entry.inv_slot then return end
+		local hasMainHandEnchant, mainHandExpiration, mainHandCharges, hasOffHandEnchant, offHandExpiration, offHandCharges, hasThrownEnchant, thrownExpiration, thrownCharges = GetWeaponEnchantInfo()
+		entry.texture = GetInventoryItemTexture("player", entry.inv_slot)
+		entry.iconframe.icon:SetTexture(entry.texture)
+		local enabled, duration, charges
+		if entry.inv_slot == 16 then
+			enabled = hasMainHandEnchant
+			if enabled then
+				expiration = mainHandExpiration/1000
+				charges = mainHandCharges
+			end
+		elseif entry.inv_slot == 17 then
+			enabled = hasOffHandEnchant
+			if enabled then
+				expiration = offHandExpiration/1000
+				charges = offHandCharges
+			end
+		elseif entry.inv_slot == 18 then
+			enabled = hasThrownEnchant
+			if enabled then
+				expiration = mainThrownExpiration/1000
+				charges = thrownCharges
+			end
+		end
+		if enabled and expiration > 0 then
+			entry.iconframe.icon:SetAlpha(cfg.alpha.found.icon)
+			entry.iconframe:SetAlpha(cfg.alpha.found.frame)
+			if cfg.desaturate then
+				entry.iconframe.icon:SetDesaturated(nil)
+			end
+			if charges and charges > 1 then
+				entry.iconframe.count:SetText(charges)
+			else
+				entry.iconframe.count:SetText("")
+			end
+			if expiration < 10 then
+				entry.iconframe.time:SetTextColor(1, 0.4, 0)
+			else
+				entry.iconframe.time:SetTextColor(1, 0.8, 0)
+			end
+			entry.iconframe.time:SetText(GetFormattedTime(expiration))
+		else
+			entry.iconframe:SetAlpha(cfg.alpha.not_found.frame)
+			entry.iconframe.icon:SetAlpha(cfg.alpha.not_found.icon)
+			entry.iconframe.time:SetText("")
+			entry.iconframe.count:SetText("")
+			entry.iconframe.time:SetTextColor(1, 0.8, 0)
+			if cfg.desaturate then
+				entry.iconframe.icon:SetDesaturated(1)
+			end
+		end
+	end
+	-- done!
+end
+
+local cUpdateAll = function(data)
+	for i=1, #data, 1 do
+		entry =  data[i]
+		if entry.enabled then
+			cUpdateEntry(entry)
+		end
+	end
+end
+
+local lastupdate = 0
+local cFilterOnUpdate = function(self,elapsed)
+	lastupdate = lastupdate + elapsed	
+	if lastupdate > cfg.updatetime then
+		lastupdate = 0
+		if cfg.track_class and cfg.IconList[cfg.player_class] then
+			cUpdateAll(cfg.IconList[cfg.player_class])
+		end
+		if cfg.track_items then
+			cUpdateAll(cfg.IconList["ITEMS"])
+		end
+		if cfg.track_enchants then
+			cUpdateAll(cfg.IconList["ENCHANTS"])
+		end
+	end
+end
+
+
+
+
+local count = 0
+
+-- create class specific icons
+if cfg.track_class and cfg.IconList[cfg.player_class] then
+	for i=1, #cfg.IconList[cfg.player_class], 1 do
+		local data = cfg.IconList[cfg.player_class][i]
+		if data.enabled then
+			createIcon(data)
+			count=count+1
+		end
+	end
+end
+
+if cfg.track_items then
+	for i=1, #cfg.IconList["ITEMS"], 1 do
+		local data = cfg.IconList["ITEMS"][i]
+		if data.enabled then
+			createIcon(data)
+			count=count+1
+		end
+	end
+end
+if cfg.track_enchants then
+	for i=1, #cfg.IconList["ENCHANTS"], 1 do
+		local data = cfg.IconList["ENCHANTS"][i]
+		if data.enabled then
+			createIcon(data)
+			count=count+1
+		end
+	end
+end
+if count > 0 then
+	
+	local a = CreateFrame("Frame")
+
+	a:SetScript("OnEvent", function(self, event)
+		if(event=="PLAYER_LOGIN") then
+			self:SetScript("OnUpdate", cFilterOnUpdate)
+		end
+	end)
+	
+	a:RegisterEvent("PLAYER_LOGIN")
+
+end
+
+
+
+
